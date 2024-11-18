c0d016d98c9844233296e51aee09e1e929bcf9b4
Artyom Osipchuk <artos4002@yandex.ru>
2024-11-18 22:39:28 +0300

    09 PatchDiff

--- 09_PatchDiff/labirinth.c
+++ 09_PatchDiff/labirinth.c
@@ -20,33 +20,39 @@ int main(int argc, char *argv[]) {
 	
 	int x = rand() % (n - 1) + 1;
 	int y = rand() % (n - 1) + 1;
-	for (int i = 1; i < n; i += 2) {
-		for (int j = 1; j < n; j += 2) {
+	for (int i = 1; i < n - 1; i += 2) {
+		for (int j = 1; j < n - 1; j += 2) {
 			int x = i, y = j;
 			int vi = rand() % 4;
 			int x_ = x + v[vi][0];
 			int y_ = y + v[vi][1];
 			while (maze[x_][y_] != wall) {
-				int vi = rand() % 4;
+				int vi = (vi + 1) % 4;
 				x_ = x + v[vi][0];
 				y_ = y + v[vi][1];
 			}
-			int c = 1;
 			maze[x_][y_] = space;
 			if (x_ == (n - 1) || y_ == (n - 1) || x_ == 0 || y_ == 0) {
 				maze[x_][y_] = wall;
-				c = -1;
-				x_ += c * v[vi][0];
-				y_ += c * v[vi][1];
+				x_ -= 2 * v[vi][0];
+				y_ -= 2 * v[vi][1];
+				maze[x_][y_] = space;
 			}
-			x_ += c * v[vi][0];
-			y_ += c * v[vi][1];
-			maze[x_][y_] = space;
+		}
+	}
+	// делать из графа дерево, через все вершины?
+	// а может лучше сделать из n * n комнат одну комнату?
+	for (int i = 1; i < n; i += 2) {
+		for (int j = 2; j < n - 1; j += 2) {
+			maze[i][j] = space;
 		}
 	}
 	
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < n; ++j) {
+			if (j == 0 || j == n - 1) {
+				maze[i][j] = wall;
+			}
 			printf("%s", maze[i][j]);
 		}
 		printf("\n");
